6fae334a6e0c80c4eba0e86c361faac13e7550c7
Roy42069360 <15274@student.eadania.dk>
2022-04-04 13:19:52 +0200

    Made Player rotate

--- Components/SpriteRenderer.cs
+++ Components/SpriteRenderer.cs
@@ -8,6 +8,8 @@ namespace DesignPatternsProjekt
         public Texture2D Sprite { get; set; }
         public Vector2 Origin { get; set; }
 
+        public float Rotation { get; set; }
+
         public override void Start()
         {
             Origin = new Vector2(Sprite.Width / 2, Sprite.Height / 2);
@@ -24,7 +26,7 @@ namespace DesignPatternsProjekt
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(Sprite, GameObject.Transform.Position, null, Color.White, 0, Origin, 1, SpriteEffects.None, 1);
+            spriteBatch.Draw(Sprite, GameObject.Transform.Position, null, Color.White, Rotation, Origin, 1, SpriteEffects.None, 1);
         }
     }
 }
