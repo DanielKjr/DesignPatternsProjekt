8a93b474014b0b806fb77d38f0fbaf36738bcb65
HCeption <15846@student.eadania.dk>
2022-04-04 12:50:42 +0200

    BackgroundStarts comments

--- BackgroundStars.cs
+++ BackgroundStars.cs
@@ -10,34 +10,37 @@ namespace DesignPatternsProjekt
     {
         private Random rnd = new Random();
         private Vector2 velocity = new Vector2(0, 1);
+        private float speed = 1f;
         private SpriteRenderer rend;
             
         public override void Start()
         {
             rend = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
             rend.SetSprite("Pixel");
-            Reset();
+            Reset(); //Prep for first launch. Reset Y value on lext line for normal startup looks.
             GameObject.Transform.Position = new Vector2(rnd.Next(0, GameWorld.Instance.Graphics.PreferredBackBufferWidth), rnd.Next(0, GameWorld.Instance.Graphics.PreferredBackBufferHeight));
         }
         private void Move()
         {
-            //velocity *= speed;
-            GameObject.Transform.Translate(velocity * GameWorld.DeltaTime);
+            GameObject.Transform.Translate(velocity * GameWorld.DeltaTime * speed); //Move star
         }
+        /// <summary>
+        /// Reset position to random X, and Y=0 to look like new star appearing.
+        /// New speed and scale values added too.
+        /// </summary>
         private void Reset()
         {
             GameObject.Transform.Position = new Vector2(rnd.Next(0, GameWorld.Instance.Graphics.PreferredBackBufferWidth), 0);
-            velocity = new Vector2(0, rnd.Next(20, 150));
-            float variance = rnd.Next(-13, 24) /100;
-            rend.Scale = (velocity.Y / 150) + variance;
+            speed = rnd.Next(20, 150);
+            rend.Scale = (speed / 150) + (rnd.Next(-15, 40) / 100); //scale depens on speed with a randomized variance added on top.
         }
         public override void Update(GameTime gameTime)
         {
-            if (GameObject.Transform.Position.Y > GameWorld.Instance.Graphics.PreferredBackBufferHeight)
+            if (GameObject.Transform.Position.Y > GameWorld.Instance.Graphics.PreferredBackBufferHeight) //If outside bottom of screen, reset.
             {
                 Reset();
             }
-            Move();
+            Move(); //If not outside, keep moving.
         }
     }
 }
