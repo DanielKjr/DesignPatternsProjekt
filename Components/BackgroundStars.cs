737c9dcc33beed37ff2ba72139da23486660e9b4
HCeption <15846@student.eadania.dk>
2022-04-05 10:46:25 +0200

    Proper sprites and new features.

--- Components/BackgroundStars.cs
+++ Components/BackgroundStars.cs
@@ -12,7 +12,7 @@ namespace DesignPatternsProjekt
         private Vector2 velocity = new Vector2(0, 1);
         private float speed = 1f;
         private SpriteRenderer rend;
-        
+
         public BackgroundStars(float _speed)
         {
             speed = _speed;
@@ -36,6 +36,10 @@ namespace DesignPatternsProjekt
             GameObject.Transform.Position = new Vector2(rnd.Next(0, GameWorld.Instance.Graphics.PreferredBackBufferWidth), 0);
             speed = rnd.Next(20, 150);
             rend.Scale = (speed / 150) + (rnd.Next(-15, 40) / 100); //scale depens on speed with a randomized variance added on top.
+            rend.SetSprite("Star" + rnd.Next(1, 4)); //New random sprite
+            rend.Rotation = rnd.Next(0, 4); //New random rotation
+            float temp = rnd.Next(-2, 2); //Didnt want to work normaly... eh.
+            velocity = new Vector2(temp / 100, 1); //new random dir, still full down but maybe a bit sideways too
         }
         public override void Update(GameTime gameTime)
         {
